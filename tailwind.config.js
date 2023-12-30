ea77bd618b7925d313c4f13a1476c90849c38fd8
Andrii Serputko <a.serputko@sap.com>
2023-12-30 10:57:39 +0100

    feat: code clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        blue: '#3A7CFD', // active state
-        white: '#FFFFFF', // todo background
-        light: '#FAFAFA', // body background
-        dark: '#494C6B', // default text color
-        'gray-500': '#9495A5', // filters text color, add todo color
-        'gray-300': '#D1D2DA', // competed
-        'gray-200': '#E3E4F1', // borders
-        'gray-100': '#FAFAFA', // body background
-
-        // DARK
-        black: '#171823', // body background
-        'slate-800': '#25273D', // todo background
-        'slate-700': '#393A4B', // borders
-        'slate-600': '#4D5067', // competed
-        'slate-500': '#767992', // filters text color, add todo color
-        'slate-200': '#C8CBE7', // default text color
+        black: '#171823',
+        dark: '#494C6B',
+        light: '#FAFAFA',
+        white: '#FFFFFF',
+        blue: '#3A7CFD',
+        'gray-500': '#9495A5',
+        'gray-300': '#D1D2DA',
+        'gray-200': '#E3E4F1',
+        'gray-100': '#FAFAFA',
+        'slate-800': '#25273D',
+        'slate-700': '#393A4B',
+        'slate-600': '#4D5067',
+        'slate-500': '#767992',
+        'slate-200': '#C8CBE7',
       },
     },
   },
